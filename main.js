a43dec109a1a6e0397647e2ba86b7fc8dd395ae0
Karsten Løgstrup <61208095+logstrup78@users.noreply.github.com>
2021-01-07 16:25:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -44,12 +44,6 @@ $(function(){
 $("h1").hide().fadeIn(4000);
 })
 
-$(".fa-lightbulb").hover(function(){
-    $(this).css("color", "#FFD700");},
-    function(){
-        $(this).css("color", "#383838")
-    })
-
     $("#collapse_target").on('show.bs.collapse', function() {
         $('a.nav-link').click(function() {
             $("#collapse_target").collapse('hide');
